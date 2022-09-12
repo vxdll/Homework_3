25163274ffd33ae9b23d37cb919303ee4077088d
Григорий Васякин <vxdll.39@gmail.com>
2022-09-12 19:07:58 +0200

    Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,7 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите N:");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i <= N; i++){
+    double result = Math.Pow(i, 3);
+    Console.WriteLine(result + " ");
+} 
